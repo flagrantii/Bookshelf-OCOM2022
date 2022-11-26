a0bf98125d1dd87e00c17e6f9010a96b9a750cd0
flagranti <118968916+flagrantii@users.noreply.github.com>
2022-11-26 15:44:47 +0700

    Add files via upload

---
+++ bookshelf.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int slove(int n,int m,int arr[]){
+	int max=-1e9;
+	for(int )
+}
+
+int main(){
+	int n,x;
+	cin>>n;
+	vector<int> s;
+	for(int i=0;i<n-1;i++){
+		cin>>s[i];
+	}
+}
